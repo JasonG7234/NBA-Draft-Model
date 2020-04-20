d242c523440a1370dcc459328b296914dcdde107
JasonG7234 <JasonG7234@gmail.com>
2020-04-19 22:21:36 -0400

    refactored graphing to utilize user input + be more universal

--- graph.py
+++ graph.py
@@ -3,18 +3,51 @@ import numpy as np
 import matplotlib.pyplot as plt
 #import statsmodels.api as sm
 
-def graph(tableName, labelRowIndex, xAxisRowIndex, yAxisRowIndex):
+graphFieldNames = ['label', 'x-axis', 'y-axis']
+
+def main():
+    master = getCSVFile()
+    columnCount = 0
+    for col in master.columns:
+        print("At index " + str(columnCount) + ", the column name is " + col)
+        columnCount += 1
+    columnIndexes = []
+    for graphFieldName in graphFieldNames:
+        columnIndexes.append(getIndexOfColumn(graphFieldName, columnCount))
+    graph(master, columnIndexes)
+
+def getCSVFile():
+    while True:
+        file_name = input("What csv file would you like to graph? ").strip()
+        try:
+            master = pd.read_csv(file_name)
+        except FileNotFoundError:
+            print("ERROR - File not found. Please try again.")
+            continue
+        return master
+
+def getIndexOfColumn(graph_part, columnCount):
+    while True:
+        try:
+            columnIndex = int(input("What column index would you like to be the " + graph_part + "? "))
+        except ValueError:
+            print("ERROR - That is not a valid column index. Please try again.")
+            continue
+        if (int(columnIndex) > columnCount or int(columnIndex) < 0):
+            print("ERROR - That column index is not within the bounds of the CSV. Please try again.")
+            continue
+        return columnIndex
+
+def graph(tableName, columnIndexes):
+    xAxisRowIndex = int(columnIndexes[1])
+    yAxisRowIndex = int(columnIndexes[2])
     plt.figure()
     plt.scatter(tableName.iloc[:, xAxisRowIndex], tableName.iloc[:, yAxisRowIndex])
-    plt.xlabel(tableName.columns[xAxisRowIndex])
-    plt.ylabel(tableName.columns[yAxisRowIndex])
+    plt.xlabel(tableName.columns[columnIndexes[1]])
+    plt.ylabel(tableName.columns[columnIndexes[2]])
     for index,row in tableName.iterrows():
-        plt.annotate(row[labelRowIndex], (row[xAxisRowIndex], row[yAxisRowIndex]))
+        plt.annotate(row[columnIndexes[0]], (row[columnIndexes[1]], row[columnIndexes[2]]))
     plt.show()
-    
-master = pd.read_csv("master.csv")
-i = 0
-for (columnName, columnData) in master.iteritems():
-    print("At index " + str(i) + ", the column name is " + columnName)
-    i += 1
-graph(master, 4, 2, 45)
+
+if __name__ == "__main__":
+    main()
