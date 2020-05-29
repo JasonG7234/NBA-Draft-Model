5bb26d62d56b5914e2165ce9502699cdfc6c82be
JasonG7234 <JasonG7234@gmail.com>
2020-05-28 23:15:13 -0400

    Updated logistic regression modeling to better predict upcoming classes

--- logreg.py
+++ logreg.py
@@ -1,6 +1,6 @@
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
-from sklearn import metrics
+from sklearn import metrics, preprocessing
 from utils import *
 import pandas as pd
 import numpy as np
@@ -11,31 +11,51 @@ def main():
 
 # Creates the logistic regression model and tests accuracy
 def performLogReg(dataframe):
-    X = pd.DataFrame()
-    # Update if new stats are added
-    Y = pd.DataFrame(index=np.arange(len(dataframe)), columns=['Result'])
-    dataframe = dataframe.replace('', np.nan)
+
+    X = pd.DataFrame() # Create main dataframe
+    Y = pd.DataFrame(index=np.arange(len(dataframe)), columns=['Result']) # Create result dataframe
+    
+    dataframe = populateDataFrameWithAverageValues(dataframe)
+
+    prospectsForUpcomingNBADraft = dataframe.loc[dataframe['NBA GP'] == '?'] # Create prospect dataframe
     for index, row in dataframe.iterrows():
-        if (row.isnull().values.all() or row['NBA GP'] == '?'):
+        if (row['NBA GP'] == '?'):
             dataframe = dataframe.drop(index)
             Y = Y.drop(index)
             continue
-        elif (row.isnull().values.any()):
-            dataframe = dataframe.fillna(dataframe.mean())
-        returnVal = isNBAPlayer(row)
-        Y.loc[index, 'Result'] = returnVal
-    X = dataframe[LOG_REG_COLUMNS] # Features
-    Y = Y.astype('int')
+        Y.loc[index, 'Result'] = isNBAPlayer(row)
 
-    X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2, shuffle=True)
-    logreg = LogisticRegression()
+    X = dataframe[LOG_REG_COLUMNS] # Tie only necessary stats to algorithm
+    prospectStatsForUpcomingNBADraft = prospectsForUpcomingNBADraft[LOG_REG_COLUMNS]
+
+    Y = Y.astype('int') # Cast all of Y to be an integer (0 or 1)
+
+    X_train, X_test, Y_train, Y_test = getTrainTestSplit(X, Y) # Run train/test split
     
+    logreg = LogisticRegression(max_iter=500)
     logreg.fit(X_train, Y_train.values.ravel())  # Fits model with data
-    Y_pred = logreg.predict(X_test)
+    Y_pred = logreg.predict(X_test) # Make predictions on test data to get baseline accuracy
+    
+    makePredictionsForUpcomingNBAProspects(logreg, prospectStatsForUpcomingNBADraft, prospectsForUpcomingNBADraft)
+    printCoefficientInformation(logreg)
+    printConfusionMatrixForTestData(Y_test, Y_pred)
 
-    confusionMatrix = metrics.confusion_matrix(Y_test, Y_pred)  # Diagonals tell you correct predictions
+    return logreg
 
-    # Code below prints model accuracy information
+def populateDataFrameWithAverageValues(df):
+    df = df.replace('', np.nan)
+    return df.fillna(df.mean())
+
+def getTrainTestSplit(X, Y):
+    X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.5, shuffle=False)
+    return X_train, X_test, Y_train, Y_test
+
+def makePredictionsForUpcomingNBAProspects(logreg, prospectStats, prospects):
+    predictions = logreg.predict(prospectStats)
+    prospects['NBA Player?'] = predictions
+    prospects.to_csv('upcomingProspects.csv', index=False)
+
+def printCoefficientInformation(logreg):
     print('Coefficient Information:')
 
     for i in range(len(LOG_REG_COLUMNS)):  # Prints each feature next to its corresponding coefficient in the model
@@ -47,19 +67,19 @@ def performLogReg(dataframe):
 
         print(currentFeature + ': ' + str(currentCoefficient))
 
+def printConfusionMatrixForTestData(test, pred):
+    confusionMatrix = metrics.confusion_matrix(test, pred)  # Diagonals tell you correct predictions
+    
     print('----------------------------------')
 
-    print("Accuracy:", metrics.accuracy_score(Y_test, Y_pred))
-    print("Precision:", metrics.precision_score(Y_test, Y_pred))
-    print("Recall:", metrics.recall_score(Y_test, Y_pred))
+    print("Accuracy:", metrics.accuracy_score(test, pred))
+    print("Precision:", metrics.precision_score(test, pred))
+    print("Recall:", metrics.recall_score(test, pred))
 
     print('----------------------------------')
 
     print('Confusion Matrix:')
     print(confusionMatrix)
-
-    return logreg
-
 
 if __name__ == "__main__":
     main()
