6582a91f53d5e3089be1b8fcbb39f04a9c98609a
JasonG7234 <JasonG7234@gmail.com>
2022-07-03 11:30:17 -0400

    fix: have measurements api calls working

--- src/nbacombineanthro/measurements.py
+++ src/nbacombineanthro/measurements.py
@@ -1,57 +1,66 @@
-from nba_api.stats.endpoints import draftcombineplayeranthro
+import sys
+sys.path.insert(0, '../../')
+from utils import *
 
-from fuzzywuzzy import fuzz
+from nba_api.stats.endpoints import draftcombineplayeranthro
+import time
+
+DRAFT_COMBINE_ANTHRO_COLUMNS = ["POSITION", "HEIGHT_WO_SHOES", "HEIGHT_W_SHOES", "HEIGHT_W_SHOES", "WEIGHT", "WINGSPAN", "STANDING_REACH", "BODY_FAT_PCT", "HAND_LENGTH", "HAND_WIDTH"]
+
+DRAFT_COMBINE_DATAFRAME_COLUMN_NAMES = ["Position", "Height w/o Shoes", "Height w/ Shoes", "Wingspan", "Weight", "Standing Reach", "Body Fat %", "Hand Length", "Hand Width"]
+
+NBA_DRAFT_COMBINE_NAME_EXCEPTIONS = {
+    "Edrice Adebayo" : "Bam Adebayo"
+    }
 
 def get_NBA_Combine_measurements(df):
     tmp = df.sort_values(by=['Season'])
-    for season in set(tmp['Season']):
-        combine_data = draftcombineplayeranthro.DraftCombinePlayerAnthro(
+    for col in DRAFT_COMBINE_DATAFRAME_COLUMN_NAMES:
+        if col not in tmp:
+            tmp[col] = ""
+            
+    for season in tmp.Season.unique():
+        print(season)
+        season_players = tmp[tmp['Season'] == season]
+        combine_data = get_NBA_combine_data(season)
+
+        for _, combine_player in combine_data.iterrows():
+            print(combine_player['PLAYER_NAME'])
+            for _, df_player in season_players.iterrows():
+                combine_player_name = check_value_in_dictionary_of_exceptions(combine_player['PLAYER_NAME'], NBA_DRAFT_COMBINE_NAME_EXCEPTIONS, combine_player['PLAYER_NAME'])
+                try:
+                    if (is_fuzzy_name_match(df_player['Name'], combine_player_name)):
+                        print(f"Found match for {combine_player['PLAYER_NAME']}")
+                        populate_NBA_combine_measurements(tmp, df.loc[(df['Name'] == df_player['Name']) & (df['School'] == df_player['School'])], combine_player)
+                        break
+                except KeyError:
+                    print(f"Could not find direct match for {combine_player['PLAYER_NAME']}")
+    return tmp
+                
+def populate_NBA_combine_measurements(df, row, combine_values):
+    print(row.index[0])
+    for i in range(len(DRAFT_COMBINE_DATAFRAME_COLUMN_NAMES)):
+        col_index = df.columns.get_loc(DRAFT_COMBINE_DATAFRAME_COLUMN_NAMES[i])
+        df.loc[row.index[0], col_index] = combine_values[DRAFT_COMBINE_ANTHRO_COLUMNS[i]]
+        
+def get_NBA_combine_data(season):
+    season = str(int(season[:4])+1) + '-' + str(int(season[-2:])+1)
+    time.sleep(2)
+    return draftcombineplayeranthro.DraftCombinePlayerAnthro(
             league_id='00', 
             season_year=season,
             headers={'Accept': 'application/json, text/plain, */*',
 'Accept-Encoding': 'gzip, deflate, br',
-'Accept-Language': 'en-US,en;q=0.9',
+'Accept-Language': 'en-US,en;q=0.5',
 'Connection': 'keep-alive',
 'Host': 'stats.nba.com',
-'Origin': 'https://www.nba.com',
-'Referer': 'https://www.nba.com/',
+'Origin': 'https://www.stats.nba.com',
+'Referer': 'https://www.stats.nba.com/',
 'sec-ch-ua': '"Google Chrome";v="87", "\"Not;A\\Brand";v="99", "Chromium";v="87"',
 'sec-ch-ua-mobile': '?1',
 'Sec-Fetch-Dest': 'empty',
 'Sec-Fetch-Mode': 'cors',
 'Sec-Fetch-Site': 'same-site',
-'User-Agent': 'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.141 Mobile Safari/537.36',
+'User-Agent': 'Mozilla/5.0 AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.141 Mobile Safari/537.36',
 'x-nba-stats-origin': 'stats',
 'x-nba-stats-token': 'true'}).results.get_data_frame()
-
-        season_players = tmp[tmp['Season'] == season]
-        for index, row in season_players.iterrows():
-            row_index = tmp.index[season_players['Name'] == row_name]
-        #     if (row_index):
-        #         print(f"Found match for {row[0].getText()}")
-        #         populate_NBA_combine_measurements(df, row_index, row)
-        #     else:
-        #         mapped_name = nba_combine_page.get_mapped_name(row_name)
-        #         tmp.index[utils.is_fuzzy_name_match(season_players['Name'], mapped_name)]
-        #         if (row_index):
-        #             populate_NBA_combine_measurements(df, row_index, row)
-        #         else:
-        #             print(f"ERROR: Unable to find match for {mapped_name}")
-    return tmp
-                
-def populate_NBA_combine_measurements(df, index, row_values):
-    row = df.iloc(index)
-    for pair in NBA_COMBINE_MEASUREMENT_COLUMNS.items():
-        if (pair[1] == 1): # Position
-            row[pair[0]] = row_values[pair[1]].getText()
-        if (pair[1] == 2): # Body Fat
-            row[pair[0]] = float(row_values[pair[1]].getText()[:-1])
-        if (pair[1] in [5, 6, 7, 9]): # Height in feet/inches
-            row[pair[0]] = translate_measurements_to_inches(row_values[pair[1]].getText())
-        else:
-            row[pair[0]] = float(row_values[pair[1]].getText())
-            
-def translate_measurements_to_inches(measurement):
-    feet = int(measurement.split("'")[0])
-    inches = float(measurement.split("'")[1].replace(' ','').replace('"', ''))
-    return feet*12+inches
