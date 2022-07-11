45f5705497d3856d74ba98b07c0cf65e9d904070
JasonG7234 <JasonG7234@gmail.com>
2022-07-11 19:31:10 -0400

    chore: test.py has a bunch of new methods

--- test.py
+++ test.py
@@ -36,79 +36,94 @@ def bentaylor_stats(main):
             print(f"{row['Name']}") 
     return main
     
-main = pd.read_csv('data/jason_db.csv')
-#draw_conclusions_on_column(main, 'USG%')
-#draw_conclusions_on_column(main, 'Offensive Load')
-st.set_page_config(page_title="NBA Draft Model", page_icon=":bar_chart:", layout="wide")
+def reorder_final_columns(main):
+    return main[['RealGM ID','Season','Name',
+                'Position 1','Position 2',
+                'School','Wins','Losses','SOS',
+                'Class','Birthday','Draft Day Age',
+                'Height','Weight','Height w/o Shoes','Height w/ Shoes','Wingspan','Standing Reach','Body Fat %','Hand Length','Hand Width',
+                'RSCI','G','GS','MP','PER','TS%','eFG%','3PAr','FTr','PProd','ORB%','DRB%','TRB%','AST%','STL%','BLK%','TOV%','USG%','OWS','DWS','WS','WS/40','OBPM','DBPM','BPM','ATS/TOV','OFF RTG','DEF RTG','Hands-On Buckets','Pure Point Rating',
+                'FG/40','FGA/40','FG%','2FGM/40','2FGA/40','2FG%','3FGM/40','3FGA/40','3FG%','FT/40','FTA/40','FT%','TRB/40','AST/40','STL/40','BLK/40','TOV/40','PF/40','PTS/40',
+                'FGM/100Poss','FGA/100Poss','2FGM/100Poss','2FGA/100Poss','3FGM/100Poss','3FGA/100Poss','FT/100Poss','FTA/100Poss','TRB/100Poss','AST/100Poss','STL/100Poss','BLK/100Poss','TOV/100Poss','PF/100Poss','PTS/100Poss','FGA/100Poss',
+                '# Dunks','% Shots @ Rim','FG% @ Rim','%Astd @ Rim','% Shots @ Mid','FG% @ Mid','%Astd @ Mid','% Shots @ 3','%Astd @ 3',
+                'AAU Season','AAU Team','AAU League','AAU GP','AAU GS','AAU MIN','AAU PTS','AAU FGM','AAU FGA','AAU FG%','AAU 3PM','AAU 3PA','AAU 3P%','AAU FTM','AAU FTA','AAU FT%','AAU ORB','AAU DRB','AAU TRB','AAU AST','AAU STL','AAU BLK','AAU TOV','AAU PF',
+                'Event Year','Event Name','Event GP','Event MIN','Event PTS','Event FGM','Event FGA','Event FG%','Event 3PM','Event 3PA','Event 3P%','Event FTM','Event FTA','Event FT%','Event TRB','Event AST','Event STL','Event BLK','Event TOV','Event PF','Event Placement'
+                ]]
 
-st.sidebar.header("Please Filter Here:")
-position = st.sidebar.multiselect(
-    "Select the primary position:",
-    options=main["Position 1"].unique(),
-    default=main["Position 1"].unique()
-)
+main = pd.read_csv('data/main.csv')
+reorder_final_columns(main).to_csv('temp_master.csv', index=False)
 
-season = st.sidebar.multiselect(
-    "Select the season:",
-    options=main["Season"].unique(),
-    default=main["Season"].unique()
-)
+def set_up_streamlit(main):
+    st.set_page_config(page_title="NBA Draft Model", page_icon=":bar_chart:", layout="wide")
 
-classification = st.sidebar.multiselect(
-    "Select the class/year:",
-    options=main["Class"].unique(),
-    default=main["Class"].unique()
-)
-st.sidebar.text("Class 1 = Freshmen, Class 2 = Sophomore, etc.")
+    st.sidebar.header("Please Filter Here:")
+    position = st.sidebar.multiselect(
+        "Select the primary position:",
+        options=main["Position 1"].unique(),
+        default=main["Position 1"].unique()
+    )
 
-main_selection = main.query(
-    "'Position 1' == @position & Season == @season & Class == @classification"
-)
-main['Position 2'] = main['Position 2'].fillna('')
-main = main.round({'PER': 1})
-st.dataframe(main)
+    season = st.sidebar.multiselect(
+        "Select the season:",
+        options=main["Season"].unique(),
+        default=main["Season"].unique()
+    )
 
-bsc = (
-    main_selection.groupby(by=["Class"]).mean()[["Box Score Creation"]].sort_values(by="Box Score Creation")
-)
-fig_bsc = px.bar(
-    bsc,
-    x="Box Score Creation",
-    y=bsc.index,
-    orientation="h",
-    title="<b>Box Score Creation by Year</b>",
-    color_discrete_sequence=["#0083B8"] * len(bsc),
-    template="plotly_white",
-)
-fig_bsc.update_layout(
-    plot_bgcolor="rgba(0,0,0,0)",
-    xaxis=(dict(showgrid=False))
-)
+    classification = st.sidebar.multiselect(
+        "Select the class/year:",
+        options=main["Class"].unique(),
+        default=main["Class"].unique()
+    )
+    st.sidebar.text("Class 1 = Freshmen, Class 2 = Sophomore, etc.")
 
-offensive_load_by_position = main_selection.groupby(by=["Position 1"]).mean()[["Offensive Load"]]
-fig_offensive_load = px.bar(
-    offensive_load_by_position,
-    x='Position 1',
-    y="Offensive Load",
-    title="<b>Offensive Load By Position</b>",
-    color_discrete_sequence=["#0083B8"] * len(offensive_load_by_position),
-    template="plotly_white",
-)
-fig_offensive_load.update_layout(
-    xaxis=dict(tickmode="linear"),
-    plot_bgcolor="rgba(0,0,0,0)",
-    yaxis=(dict(showgrid=False)),
-)
+    main_selection = main.query(
+        "'Position 1' == @position & Season == @season & Class == @classification"
+    )
+    main['Position 2'] = main['Position 2'].fillna('')
+    main = main.round({'PER': 1})
+    st.dataframe(main)
 
-left_column, right_column = st.columns(2)
-left_column.plotly_chart(fig_offensive_load, use_container_width=True)
-right_column.plotly_chart(fig_bsc, use_container_width=True)
-#main.to_csv('temp_master.csv', index=False)
-hide_st_style = """
-            <style>
-            #MainMenu {visibility: hidden;}
-            footer {visibility: hidden;}
-            header {visibility: hidden;}
-            </style>
-            """
-st.markdown(hide_st_style, unsafe_allow_html=True)
+    bsc = (
+        main_selection.groupby(by=["Class"]).mean()[["Box Score Creation"]].sort_values(by="Box Score Creation")
+    )
+    fig_bsc = px.bar(
+        bsc,
+        x="Box Score Creation",
+        y=bsc.index,
+        orientation="h",
+        title="<b>Box Score Creation by Year</b>",
+        color_discrete_sequence=["#0083B8"] * len(bsc),
+        template="plotly_white",
+    )
+    fig_bsc.update_layout(
+        plot_bgcolor="rgba(0,0,0,0)",
+        xaxis=(dict(showgrid=False))
+    )
+
+    offensive_load_by_position = main_selection.groupby(by=["Position 1"]).mean()[["Offensive Load"]]
+    fig_offensive_load = px.bar(
+        offensive_load_by_position,
+        x='Position 1',
+        y="Offensive Load",
+        title="<b>Offensive Load By Position</b>",
+        color_discrete_sequence=["#0083B8"] * len(offensive_load_by_position),
+        template="plotly_white",
+    )
+    fig_offensive_load.update_layout(
+        xaxis=dict(tickmode="linear"),
+        plot_bgcolor="rgba(0,0,0,0)",
+        yaxis=(dict(showgrid=False)),
+    )
+
+    left_column, right_column = st.columns(2)
+    left_column.plotly_chart(fig_offensive_load, use_container_width=True)
+    right_column.plotly_chart(fig_bsc, use_container_width=True)
+    #main.to_csv('temp_master.csv', index=False)
+    hide_st_style = """
+                <style>
+                #MainMenu {visibility: hidden;}
+                footer {visibility: hidden;}
+                header {visibility: hidden;}
+                </style>
+                """
+    st.markdown(hide_st_style, unsafe_allow_html=True)
